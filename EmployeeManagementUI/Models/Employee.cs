8dc7666b90d8cb82d0cce553c3ee9333bfd998f1
Libitha Benny <libitha.benny@milestone.tech>
2026-09-01 15:45:09 +0530

    Completed Blazor CRUD application

---
+++ EmployeeManagementUI/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿namespace EmployeeManagementUI.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; } = "";
+
+        [Required]
+        public string LastName { get; set; } = "";
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = "";
+
+        [Range(1, 1000000)]
+        public decimal Salary { get; set; }
+    }
+}
---
+++ EmployeeManagementUI/Services/EmployeeService.cs
@@ -0,0 +1,49 @@
+﻿using EmployeeManagementUI.Models;
+using System.Net.Http.Json;
+
+namespace EmployeeManagementUI.Services
+{
+    public class EmployeeService
+    {
+        private readonly HttpClient _httpClient;
+
+        public EmployeeService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<Employee>> GetEmployees()
+        {
+            return await _httpClient.GetFromJsonAsync<List<Employee>>
+            (
+                "https://localhost:7040/api/employee"
+            ) ?? new List<Employee>();
+        }
+
+        public async Task CreateEmployee(Employee employee)
+        {
+            await _httpClient.PostAsJsonAsync(
+                "https://localhost:7040/api/employee",
+                employee);
+        }
+
+        public async Task<Employee?> GetEmployeeById(int id)
+        {
+            return await _httpClient.GetFromJsonAsync<Employee>(
+                $"https://localhost:7040/api/employee/{id}");
+        }
+
+        public async Task UpdateEmployee(int id, Employee employee)
+        {
+            await _httpClient.PutAsJsonAsync(
+                $"https://localhost:7040/api/employee/{id}",
+                employee);
+        }
+
+        public async Task DeleteEmployee(int id)
+        {
+            await _httpClient.DeleteAsync(
+                $"https://localhost:7040/api/employee/{id}");
+        }
+    }
+}
